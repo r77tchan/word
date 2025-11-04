007c2dc9e4bc95132ae7b74ebf065d40797921d8
r77tchan <r77tchan@gmail.com>
2025-11-04 16:29:53 +0900

    プロジェクト設定ファイルを追加

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
       <div className="h-96 w-full bg-gray-300"></div>
-      <div></div>
+      <div className="mx-auto h-10 max-w-7xl bg-amber-200"></div>
       <div className="mt-32 flex justify-center gap-5">
         <Link href="/lesson" className="bg-red-300 p-4">
           レッスン
