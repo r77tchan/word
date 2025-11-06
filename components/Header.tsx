9d5742f927f8de90fcfa62303a471b9fe704af07
r77tchan <r77tchan@gmail.com>
2025-11-06 13:11:36 +0900

    スタイル調整

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,14 +5,20 @@ export default function Header() {
     <header className="bg-bar text-white select-none">
       <div className="container mx-auto flex items-center justify-between">
         <div className="text-lg font-bold">
-          <Link href="/" className="block p-4 hover:bg-gray-500">
+          <Link
+            href="/"
+            className="block p-4 hover:bg-gray-500 active:bg-gray-500"
+          >
             英単語アプリ
           </Link>
         </div>
         <nav className="">
           <ul className="flex">
             <li>
-              <Link href="/" className="block p-4 text-lg hover:bg-gray-500">
+              <Link
+                href="/"
+                className="block p-4 text-lg hover:bg-gray-500 active:bg-gray-500"
+              >
                 ログイン
               </Link>
             </li>
